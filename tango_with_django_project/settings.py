e385dc25e6e31bde0b6d6c04d7df4fc6699da88b
Blake Hall <bhall@reed.edu>
2014-01-28 11:39:13 -0800

    Basically done with Chapter 5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -2,7 +2,7 @@ import os
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
-
+DATABASE_PATH = os.path.join(PROJECT_PATH, 'rango.db')
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 
 
@@ -22,13 +22,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
     }
 }
 
@@ -135,7 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rango',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
